c8a8027a842b05742aeca41497ab16199624be0f
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2021-12-10 15:28:22 +0700

    add search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import image from './images/naruto.png';
+
+const baseUrl = 'https://api.jikan.moe/v3/search/anime'
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [animes, setAnimes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  async function searchAnime(e){
+    e.preventDefault()
+    if(!query) return;
+    try{
+      setLoading(true);
+      const response = await window.fetch(`${baseUrl}?q=${query}`);
+      const data = await response.json();
+      console.log('data', data)
+      setAnimes(data.results)
+      setLoading(false);
+    }catch(e){
+      setLoading(false);
+      console.error(e)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={image} alt="naruto" className="logo" />
+      </div>
+      <form className="form-wrapper" onSubmit={searchAnime}>
+        <input 
+          type="text" 
+          placeholder="Search here..." 
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <div style={{ marginTop: '30px' }}>loading...</div>}
+      <div className="container-anime">
+        {!loading && animes && animes.map(anime => (
+          <div className="anime-card" key={anime.mal_id}>
+            <img src={anime.image_url} alt={anime.title} className="image"/>
+            <div className="anime-detail">
+              <h3>{anime.title}</h3>
+              <p>{anime.synopsis}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
